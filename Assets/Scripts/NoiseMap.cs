4a65110747f549862a51627f5bae47622c4d26e9
Aegistus <joe.ballanders@gmail.com>
2021-03-20 17:49:03 -0400

    Fix Noise Map Returning Only One Value

--- Assets/Scripts/NoiseMap.cs
+++ Assets/Scripts/NoiseMap.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseMap
 {
-    public int xSize;
-    public int zSize;
     public float noiseScale = 1;
     public int octaves = 3;
+    [Tooltip("Controls increase in frequency of octaves")]
+    [Range(0f, 1f)]
     public float lacunarity = 1;
+    [Tooltip("Controls decrease in amplitude of octaves")]
+    [Range(0f, 1f)]
     public float persistance = 1;
 
-    public void SetSize(int xSize, int zSize)
-    {
-        this.xSize = xSize;
-        this.zSize = zSize;
-    }
+    public float XRandomOffset { get; set; }
+    public float ZRandomOffset { get; set; }
 
     public float GetPerlinValueAtPosition(float x, float z)
     {
@@ -25,7 +24,7 @@ public class NoiseMap
         {
             float frequency = Mathf.Pow(lacunarity, o);
             float amplitude = Mathf.Pow(persistance, o);
-            noiseAtPosition += Mathf.PerlinNoise((x / noiseScale + amplitude) * frequency, (z / noiseScale + amplitude) * frequency);
+            noiseAtPosition += Mathf.PerlinNoise(x + XRandomOffset / noiseScale * frequency, z + ZRandomOffset / noiseScale * frequency) * amplitude;
         }
         return noiseAtPosition;
     }
--- Assets/Scripts/TerrainConstructor.cs
+++ Assets/Scripts/TerrainConstructor.cs
@@ -12,47 +12,22 @@ public class TerrainConstructor : MonoBehaviour
     [Header("Tile Settings")]
     public int tileSize = 100;
 
-    [Header("Elevation Settings")]
-    public float elevationNoiseScale = .003f;
-    public int octaves = 3;
-    [Tooltip("Controls increase in frequency of octaves")]
-    public float elevationLacunarity = 1f;
-    [Tooltip("Controls decrease in amplitude of octaves")]
-    public float elevationPersistance = 1f;
-
     [Header("Ocean Settings")]
     [Range(0f, 1f)]
     public float seaLevel = .4f;
 
-    [Header("Mountain Settings")]
-    public float mountainSpacing = 30f;
-    [Range(0f, 1f)]
-    public float smallStart = .4f;
-    [Range(0f, 1f)]
-    public float mediumStart = .6f;
-    [Range(0f, 1f)]
-    public float largeStart = .8f;
-    public float yPosition = -3f;
-
-    [Header("River Settings")]
-    [Range(0f, 1f)]
-    public float riverMax = .1f;
-
     private int xTileTotal;
     private int zTileTotal;
     private GameObject[,] generatedTiles;
-    private float xElevationNoise;
-    private float zElevationNoise;
     private bool reconstructTerrain = false;
 
     private void Start()
     {
-        elevationNoiseMap.SetSize(settings.xSize, settings.zSize);
         xTileTotal = settings.xSize / tileSize;
         zTileTotal = settings.zSize / tileSize;
         generatedTiles = new GameObject[xTileTotal, zTileTotal];
-        xElevationNoise = Random.Range(0, 10000);
-        zElevationNoise = Random.Range(0, 10000);
+        elevationNoiseMap.XRandomOffset = Random.Range(0, 10000);
+        elevationNoiseMap.ZRandomOffset = Random.Range(0, 10000);
         StartCoroutine(PlaceTileGrid());
         //StartCoroutine(PlaceMountains());
     }
@@ -82,59 +57,59 @@ public class TerrainConstructor : MonoBehaviour
         }
     }
 
-    public IEnumerator PlaceMountains()
-    {
-        for (int x = 0; x < settings.xSize; x += (int)mountainSpacing)
-        {
-            for (int z = 0; z < settings.zSize; z += (int)mountainSpacing)
-            {
-                float noise = Mathf.PerlinNoise((x + xElevationNoise) * elevationNoiseScale, (z + zElevationNoise) * elevationNoiseScale);
-                Vector3 positionWithOffset = new Vector3(Random.Range(-mountainSpacing / 2, mountainSpacing / 2), 0, Random.Range(-mountainSpacing / 2, mountainSpacing / 2));
-                positionWithOffset += new Vector3(x, transform.position.y + yPosition, z);
-                positionWithOffset = new Vector3(Mathf.Clamp(positionWithOffset.x, 0, settings.xSize), positionWithOffset.y, Mathf.Clamp(positionWithOffset.z, 0, settings.zSize));
-                Quaternion randomRotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360f), 0));
-                GameObject mountainVariant = null;
-                if (noise >= smallStart && noise < mediumStart)
-                {
-                    mountainVariant = mountainSet.smallMountains[Random.Range(0, mountainSet.smallMountains.Length)];
-                }
-                else if (noise >= mediumStart && noise < largeStart)
-                {
-                    mountainVariant = mountainSet.mediumMountains[Random.Range(0, mountainSet.mediumMountains.Length)];
-                }
-                else if (noise >= largeStart)
-                {
-                    mountainVariant = mountainSet.largeMountains[Random.Range(0, mountainSet.largeMountains.Length)];
-                }
-                if (mountainVariant != null)
-                {
-                    Instantiate(mountainVariant, positionWithOffset,
-                        randomRotation, transform);
-                }
-                yield return null;
-            }
-        }
-    }
+    //public IEnumerator PlaceMountains()
+    //{
+    //    for (int x = 0; x < settings.xSize; x += (int)mountainSpacing)
+    //    {
+    //        for (int z = 0; z < settings.zSize; z += (int)mountainSpacing)
+    //        {
+    //            float noise = Mathf.PerlinNoise((x + xElevationNoise) * elevationNoiseScale, (z + zElevationNoise) * elevationNoiseScale);
+    //            Vector3 positionWithOffset = new Vector3(Random.Range(-mountainSpacing / 2, mountainSpacing / 2), 0, Random.Range(-mountainSpacing / 2, mountainSpacing / 2));
+    //            positionWithOffset += new Vector3(x, transform.position.y + yPosition, z);
+    //            positionWithOffset = new Vector3(Mathf.Clamp(positionWithOffset.x, 0, settings.xSize), positionWithOffset.y, Mathf.Clamp(positionWithOffset.z, 0, settings.zSize));
+    //            Quaternion randomRotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360f), 0));
+    //            GameObject mountainVariant = null;
+    //            if (noise >= smallStart && noise < mediumStart)
+    //            {
+    //                mountainVariant = mountainSet.smallMountains[Random.Range(0, mountainSet.smallMountains.Length)];
+    //            }
+    //            else if (noise >= mediumStart && noise < largeStart)
+    //            {
+    //                mountainVariant = mountainSet.mediumMountains[Random.Range(0, mountainSet.mediumMountains.Length)];
+    //            }
+    //            else if (noise >= largeStart)
+    //            {
+    //                mountainVariant = mountainSet.largeMountains[Random.Range(0, mountainSet.largeMountains.Length)];
+    //            }
+    //            if (mountainVariant != null)
+    //            {
+    //                Instantiate(mountainVariant, positionWithOffset,
+    //                    randomRotation, transform);
+    //            }
+    //            yield return null;
+    //        }
+    //    }
+    //}
 
-    bool[,] potentialStarts;
-    public IEnumerator PlaceRivers()
-    {
-        potentialStarts = new bool[xTileTotal, zTileTotal];
-        for (int x = 0; x < xTileTotal; x++)
-        {
-            for (int z = 0; z < zTileTotal; z++)
-            {
-                float noise = Mathf.PerlinNoise((x * tileSize + xElevationNoise) * elevationNoiseScale, (z * tileSize + zElevationNoise) * elevationNoiseScale);
-                if (noise < riverMax)
-                {
-                    potentialStarts[x, z] = true;
-                    generatedTiles[x, z].SetActive(false);
-                    generatedTiles[x, z] = null;
-                }
-            }
-        }
-        yield return null;
-    }
+    //bool[,] potentialStarts;
+    //public IEnumerator PlaceRivers()
+    //{
+    //    potentialStarts = new bool[xTileTotal, zTileTotal];
+    //    for (int x = 0; x < xTileTotal; x++)
+    //    {
+    //        for (int z = 0; z < zTileTotal; z++)
+    //        {
+    //            float noise = Mathf.PerlinNoise((x * tileSize + xElevationNoise) * elevationNoiseScale, (z * tileSize + zElevationNoise) * elevationNoiseScale);
+    //            if (noise < riverMax)
+    //            {
+    //                potentialStarts[x, z] = true;
+    //                generatedTiles[x, z].SetActive(false);
+    //                generatedTiles[x, z] = null;
+    //            }
+    //        }
+    //    }
+    //    yield return null;
+    //}
 
     private void OnValidate()
     {
